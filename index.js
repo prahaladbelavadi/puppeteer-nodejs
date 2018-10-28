25489475ce377d457fc8691c0c5c7324124c9cba
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-10-28 06:36:57 +0530

    edit

---
+++ index.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer')
+
+void(async()=>{
+    try{
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+    
+        await goto('https://scrapethissite.com/pages/forms')
+
+        await page.screenshot({
+            path: './screenshots/page1.png'
+        })
+        await page.pdf({path:'./pdfs/page1.pdf'})
+
+    await browser.close()
+    }catch(error){
+        console.log(error);
+        
+    }
+})
